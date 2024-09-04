6c44fc92194218cd242039ae2775e5a5a948c215
Muspi Merol <wjj123345567@icloud.com>
2024-09-04 21:47:45 +0800

    feat: refine file drop zone component

---
+++ src/lib/components/Drop.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+  import Dropzone from "svelte-file-dropzone";
+
+  const pendingSources: Record<string, string> = {};
+
+  async function handleDrop({ detail: { acceptedFiles } }: CustomEvent<any>) {
+    const files = acceptedFiles as (File & { path: string })[];
+
+    await Promise.all(files.map(async (file) => {
+      pendingSources[file.path.replace(/^\//, "")] = await file.text();
+    }));
+  }
+
+  const dispatch = createEventDispatcher<{ collected: typeof pendingSources }>();
+</script>
+
+<div class="flex flex-col gap-3 sm:max-w-2/3">
+
+  <h1 class="text-lg">从本地选择文件开始</h1>
+
+  <Dropzone class="grid h-fit min-h-40 min-w-xs w-full place-items-center rounded p-4 text-neutral-5 ring-(1.2 neutral-8) sm:min-w-md" disableDefaultStyles on:drop={handleDrop}>
+    <ol class="h-fit flex flex-row flex-wrap gap-1.5 text-xs">
+      {#each Object.keys(pendingSources) as path}
+        <li class="contents">
+          <div class="border-1 border-neutral-7 rounded-sm p-1.5 text-neutral-3 font-normal">
+            {path}
+          </div>
+        </li>
+      {:else}
+        <li class="text-sm text-neutral-5">
+          拖放或者点击以选择文件
+        </li>
+      {/each}
+    </ol>
+  </Dropzone>
+
+  <button on:click={() => dispatch("collected", pendingSources)} class="w-full flex flex-row justify-center rounded-md bg-neutral-8 p-3 transition hover:(bg-neutral-3 text-neutral-8)">
+    <div class="i-tabler-thumb-up-filled" />
+  </button>
+
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,6 +18,7 @@
         <FeatureCard icon="i-mdi-github-face" href="/github" title="打开一个 GitHub 仓库" description="从指定的 GitHub 仓库打开一个工作区" />
         <FeatureCard icon="i-material-symbols-create-new-folder-outline-rounded" href="/new" title="从零开始" description="打开一个崭新的工作区" />
         <FeatureCard icon="i-ic-twotone-timelapse" href="/recents" title="最近打开" description="打开过的工作区" />
+        <FeatureCard icon="i-icon-park-twotone-new-computer" href="/start" title="从本地选取文件" description="或者拖拽" />
       </div>
     </div>
   </div>
