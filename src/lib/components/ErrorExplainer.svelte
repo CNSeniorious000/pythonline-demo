21d68e867bece184a8a5d901ce7bafd313c821cb
Muspi Merol <me@promplate.dev>
2024-10-16 15:06:23 +0000

    feat: display skelton when awaiting LLM response fix: error explainer should be scrollable

--- src/lib/components/ErrorExplainer.svelte
+++ src/lib/components/ErrorExplainer.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import Notebook from "./notebook/Notebook.svelte";
   import WithTooltip from "./reusable/WithTooltip.svelte";
+  import SkeletonLine from "./SkeletonLine.svelte";
   import { draggable } from "@neodrag/svelte";
   import { explain } from "$lib/pyodide/api/explain";
   import { onMount } from "svelte";
@@ -39,8 +40,8 @@
 </script>
 
 <div class="mt-10 h-full max-w-[calc(100vw-40px)] w-full sm:mt-30vh lg:max-w-xl sm:max-w-lg sm:w-3/4">
-  <div in:scale|global={{ start: 0.9, easing: cubicOut }} out:scale|global={{ start: 0.95, easing: cubicIn }} use:draggable={{ applyUserSelectHack: false, cancel: ref, bounds: { left: 20, top: 20, right: 20, bottom: 20 } }} class="max-h-[calc(100vh-40px)] cursor-grab rounded bg-neutral-8/60 ring-0.9 ring-white/20 backdrop-blur-lg xl:max-w-3xl lg:ring-1.3 sm:ring-1.1" class:!pb-0={output}>
-    <div class="h-full w-full flex flex-col gap-3 bg-neutral-9/40 p-3 sm:gap-4 lg:p-4 sm:p-3.5">
+  <div in:scale|global={{ start: 0.9, easing: cubicOut }} out:scale|global={{ start: 0.95, easing: cubicIn }} use:draggable={{ applyUserSelectHack: false, cancel: ref, bounds: { left: 20, top: 20, right: 20, bottom: 20 } }} class="cursor-grab rounded bg-neutral-8/60 ring-0.9 ring-white/20 backdrop-blur-lg xl:max-w-3xl lg:ring-1.3 sm:ring-1.1">
+    <div class="max-h-[calc(100vh-40px)] w-full flex flex-col gap-3 overflow-y-hidden bg-neutral-9/40 p-3 sm:gap-4 lg:p-4 sm:p-3.5" class:!pb-0={!error}>
       <div class="flex flex-row select-none items-center justify-between">
         <div class="flex flex-row items-center gap-1.5 rounded bg-emerald-3/10 px-1.5 py-1 text-emerald-3/80">
           <div class="i-majesticons-lightbulb-shine text-base text-emerald-3" />
@@ -66,10 +67,13 @@
           <div class="overflow-x-scroll overflow-x-scroll rounded bg-orange-3/5 text-xs text-orange-3">
             <pre class="px-2.5 py-2 font-mono">{error.message}</pre>
           </div>
-        {/if}
-        {#if output}
+        {:else}
           <div class="overflow-y-scroll pb-2.5 lg:pb-3.5 sm:pb-3 [&>article_:where(h1,h2,h3,h4,h5,h6)]:(text-1.1em font-400) [&>article]:(<sm:text-xs lg:text-base)">
-            <Notebook text={output} />
+            {#if output}
+              <Notebook text={output} />
+            {:else}
+              <SkeletonLine />
+            {/if}
           </div>
         {/if}
       </div>
---
+++ src/lib/components/SkeletonLine.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { onMount } from "svelte";
+
+  let mounted = false;
+
+  onMount(() => {
+    requestAnimationFrame(() => mounted = true);
+  });
+</script>
+
+<div class="relative h-1em w-full animate-pulse overflow-hidden rounded">
+  <div class="absolute inset-0 animate-(fade-in duration-300) rounded bg-neutral-1/10 transition-transform-1000" class:-translate-x-full={!mounted} />
+</div>
